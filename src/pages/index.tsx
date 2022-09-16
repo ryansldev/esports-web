f59462ff20bce41de34a9d58871734e3cabce4fb
ryansldev <ryanlima1309@gmail.com>
2022-09-15 21:53:29 -0300

    :rocket: added initial part of landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <main></main>
+    <main className="max-w-[1344px] mx-auto flex flex-col items-center mt-20">
+      <Image
+        src="/assets/nlw-esports-logo.svg"
+        width={286}
+        height={160}
+        alt="NLW eSports Logo"
+      />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-nlw-gradient text-transparent bg-clip-text">
+          duo
+        </span> {" "}
+        está aqui.
+      </h1>
+    </main>
   )
 }
 
