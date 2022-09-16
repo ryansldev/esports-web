31a0172df8b057d1745a9dcd1992033411a2b2ab
ryansldev <ryanlima1309@gmail.com>
2022-09-15 21:53:08 -0300

    :purple_heart: add nlw-gradient to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/assets/background-galaxy.png')"
+        galaxy: "url('/assets/background-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 68%)",
       }
     },
   },
