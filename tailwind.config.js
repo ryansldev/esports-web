a1f6ea7cb6ec52f7d5d9a1f2685ff9cee87eb211
ryansldev <ryanlima1309@gmail.com>
2022-09-15 22:18:21 -0300

    :purple_heart: update font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/assets/background-galaxy.png')",
