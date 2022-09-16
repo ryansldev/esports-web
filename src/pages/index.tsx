c32719b02adea49b4658173a1021b05acb34fbac
ryansldev <ryanlima1309@gmail.com>
2022-09-15 22:03:07 -0300

    :purple_heart: add games section to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,15 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
+  const games = [
+    "League of Legends",
+    "Dota 2",
+    "Counter Strike",
+    "World of Warcraft",
+    "Apex Legends",
+    "Fortnite",
+  ];
+
   return (
     <main className="max-w-[1344px] mx-auto flex flex-col items-center mt-20">
       <Image
@@ -18,6 +27,24 @@ const Home: NextPage = () => {
         </span> {" "}
         está aqui.
       </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game, index) => (
+          <a key={index + 1} href="#" className="relative rounded-lg overflow-hidden">
+            <Image
+              src={`/assets/image-${index + 1}.png`}
+              alt={`Capa do jogo ${game}`}
+              width={180}
+              height={240}
+            />
+
+            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-1.5 left-0 right-0 rounded-md">
+              <strong className="font-bold text-white block">{game}</strong>
+              <span className="text-zinc-300 text-sm block">{index + 2} anúncios</span>
+            </div>
+          </a>
+        ))}
+      </div>
     </main>
   )
 }
