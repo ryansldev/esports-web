3e4d0fbf782edee742d545cc61d5c453ab9919b2
ryansldev <ryanlima1309@gmail.com>
2022-09-15 22:18:50 -0300

    :purple_heart: add call to action div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
 
+import { MagnifyingGlassPlus } from "phosphor-react";
+
 const Home: NextPage = () => {
   const games = [
     "League of Legends",
@@ -44,6 +46,20 @@ const Home: NextPage = () => {
             </div>
           </a>
         ))}
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </main>
   )
